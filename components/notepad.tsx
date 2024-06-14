b7d5636c7ea6b7ce7438240e9ffc437750b39caf
alanagoyal <alanaan@gmail.com>
2024-06-13 19:05:14 -0700

    add supabase

--- components/notepad.tsx
+++ components/notepad.tsx
@@ -1,6 +1,7 @@
 import { Textarea } from "./ui/textarea";
 import { Input } from "./ui/input";
 import Sidebar from "./sidebar";
+import MainHeader from "./main-header";
 
 export default function Notepad() {
   return (
@@ -8,10 +9,7 @@ export default function Notepad() {
       <div className="flex">
         <Sidebar />
         <main className="flex-1 p-5">
-          <div className="bg-[#1e1e1e] pb-4 mb-4">
-            <Input placeholder="Alana's notes" />
-            <p className="text-gray-400">March 4, 2024 at 5:35 PM</p>
-          </div>
+          <MainHeader />
           <Textarea className="bg-[#1e1e1e] h-full">
             hi, welcome to my website/notepad
           </Textarea>
