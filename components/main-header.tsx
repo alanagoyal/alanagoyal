b68d6c8da5759b4a10abaad4750d3e5469822471
alanagoyal <alanaan@gmail.com>
2024-06-14 12:24:00 -0700

    abstract title and date

--- components/main-header.tsx
+++ components/main-header.tsx
@@ -1,14 +1,14 @@
 import { PlusCircle } from "lucide-react";
 import { Input } from "./ui/input";
 
-export default function MainHeader() {
+export default function MainHeader({ title, date }: { title: string, date: string }) {
   return (
     <div className="bg-[#1e1e1e] pb-4 mb-4">
       <div className="flex items-center justify-between">
-        <Input placeholder="Alana's notes" />
+        <p className="text-lg font-bold">{title}</p>
         <PlusCircle className="w-4 h-4" />
       </div>
-      <p className="text-gray-4000">March 4, 2024 at 5:35 PM</p>
+      <p className="text-muted-foreground text-sm">{date}</p>
     </div>
   );
 }
--- components/notepad.tsx
+++ components/notepad.tsx
@@ -2,9 +2,20 @@ import { Textarea } from "./ui/textarea";
 import MainHeader from "./main-header";
 
 export default function Notepad() {
+  function formatDate(date: Date): string {
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true
+    };
+    return new Intl.DateTimeFormat('en-US', options).format(date).replace(/,\s(?=\d{1,2}:)/, ' at');
+  }
   return (
     <div>
-      <MainHeader />
+      <MainHeader title="Alana's notes" date={formatDate(new Date())} />
       <Textarea className="bg-[#1e1e1e] h-full">
         hi, welcome to my website/notepad
       </Textarea>
