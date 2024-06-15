0533f8ad52dd1285f9ff253459c092f6b8afc028
alanagoyal <alanaan@gmail.com>
2024-06-15 08:42:44 -0600

    implement home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+import Note from "@/components/note";
+import { v4 as uuidv4 } from 'uuid';
+
 export default function Home() {
+  const note = {
+    id: uuidv4(),
+    title: 'welcome!',
+    content: 'welcome to my notes',
+    created_at: new Date().toISOString(),
+    emoji: '👋🏼',
+    public: true
+  };
+
   return (
-    <main className="w-full flex-1 p-5">
-    </main>
+    <div className="w-full min-h-screen p-5">
+      <Note note={note} />
+    </div>
   );
 }
