bfe4bf269e3677d43e7ef066c51f3eb10e58d66b
alanagoyal <alanaan@gmail.com>
2024-06-17 09:00:01 -0700

    change id to slug

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,9 +1,9 @@
 import Note from "@/components/note";
 import { createClient } from "@/utils/supabase/server";
 
-export default async function NotePage({ params }: { params: { id: string } }) {
+export default async function NotePage({ params }: { params: { slug: string } }) {
   const supabase = createClient();
-  const slug = params.id
+  const slug = params.slug;
   const { data: note } = await supabase
     .from("notes")
     .select("*")
