70a4b0db8b3f93953fc849bc090d06015fb83c2a
alanagoyal <alanaan@gmail.com>
2024-06-17 08:28:15 -0700

    add markdown styling

--- components/note-content.tsx
+++ components/note-content.tsx
@@ -3,7 +3,7 @@ import ReactMarkdown from "react-markdown";
 export default function NoteContent({ note }: { note: any }) {
   return (
     <div className="bg-[#1e1e1e] h-full text-sm">
-      <ReactMarkdown>{note.content}</ReactMarkdown>
+      <ReactMarkdown className="markdown-body">{note.content}</ReactMarkdown>
     </div>
   );
 }
