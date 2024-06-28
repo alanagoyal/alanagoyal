1b068a30b4949bcb2728659c60033074d37f3ac3
alanagoyal <alanaan@gmail.com>
2024-06-28 11:36:09 -0700

    name

--- config/site.ts
+++ config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  name: "alana goyal | notes",
+  name: "alana goyal",
   description: "founder, managing partner, & engineer @ basecase",
   url: "https://www.alanagoyal.com",
   og: "https://res.cloudinary.com/djp21wtxm/image/upload/v1719596609/i1200x630-KuXbKV3iaoFo_ytauuo.png"
