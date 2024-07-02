296a0f645d24f35259f58733358a9b8cdba477d2
alanagoyal <alanaan@gmail.com>
2024-07-02 11:15:24 -0700

    make static

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,8 +1,18 @@
 import Note from "@/components/note";
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@supabase/supabase-js";
+
+
+export async function generateStaticParams() {
+  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!);
+  const { data: notes } = await supabase.from("notes").select("slug");
+  
+  return notes?.map((note) => ({
+    slug: note.slug,
+  })) || [];
+}
 
 export default async function NotePage({ params }: { params: { slug: string } }) {
-  const supabase = createClient();
+  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!);
   const slug = params.slug;
   const { data: note } = await supabase
     .from("notes")
