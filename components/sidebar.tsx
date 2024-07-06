4ba16b744cdf0cd633fd6c1794bb757f97307322
alanagoyal <alanaan@gmail.com>
2024-07-06 09:12:09 -0700

    note utils

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -7,6 +7,7 @@ import { Pin } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { CommandMenu } from "./command-menu";
 import { SidebarContent } from './sidebar-content';
+import { groupNotesByCategory, sortGroupedNotes } from '@/lib/note-utils';
 
 const labels = {
   pinned: (
@@ -98,58 +99,6 @@ export default function Sidebar({
   );
   const groupedNotes = groupNotesByCategory(userSpecificNotes, pinnedNotes);
   sortGroupedNotes(groupedNotes);
-
-  function groupNotesByCategory(notes: any[], pinnedNotes: Set<string>) {
-    const groupedNotes: any = {
-      pinned: [],
-    };
-
-    notes.forEach((note) => {
-      if (pinnedNotes.has(note.slug)) {
-        groupedNotes.pinned.push(note);
-        return;
-      }
-
-      let category = note.category;
-      if (!note.public) {
-        const createdDate = new Date(note.created_at);
-        const today = new Date();
-        const yesterday = new Date(today);
-        yesterday.setDate(yesterday.getDate() - 1);
-        const sevenDaysAgo = new Date(today);
-        sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
-        const thirtyDaysAgo = new Date(today);
-        thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
-
-        if (createdDate.toDateString() === today.toDateString()) {
-          category = "today";
-        } else if (createdDate.toDateString() === yesterday.toDateString()) {
-          category = "yesterday";
-        } else if (createdDate > sevenDaysAgo) {
-          category = "7";
-        } else if (createdDate > thirtyDaysAgo) {
-          category = "30";
-        } else {
-          category = "older";
-        }
-      }
-
-      if (!groupedNotes[category]) {
-        groupedNotes[category] = [];
-      }
-      groupedNotes[category].push(note);
-    });
-
-    return groupedNotes;
-  }
-
-  function sortGroupedNotes(groupedNotes: any) {
-    Object.keys(groupedNotes).forEach((category) => {
-      groupedNotes[category].sort((a: any, b: any) =>
-        b.created_at.localeCompare(a.created_at)
-      );
-    });
-  }
 
   const router = useRouter();
 
