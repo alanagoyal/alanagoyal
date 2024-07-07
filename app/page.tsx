9742632e590c85ac28e0612d5f3499177e5e23ef
alanagoyal <alanaan@gmail.com>
2024-07-07 15:03:40 -0700

    fix /

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,12 @@
+import { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "notes",
+    openGraph: {
+      images: [`/api/og/?title=${encodeURIComponent("notes")}&emoji=${encodeURIComponent("")}`],
+    },
+  };
+}
+
 export default async function Home() {}
