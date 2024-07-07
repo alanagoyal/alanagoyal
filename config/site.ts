c922368b5e61957af779f76519af53c220526944
alanagoyal <alanaan@gmail.com>
2024-07-07 10:01:05 -0700

    change url

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,6 @@ export const siteConfig = {
   name: "alana goyal",
   title: "alana goyal | notes",
   description: "founder, managing partner, & engineer @ basecase",
-  url: "https://alanagoyal-git-dynamic-og-alanagoyals-projects.vercel.app",
-  og: "https://res.cloudinary.com/djp21wtxm/image/upload/v1719596609/i1200x630-KuXbKV3iaoFo_ytauuo.png"
+  url: "https://alanagoyal.com",
 };
 
