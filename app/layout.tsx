78ba42a2af175ba00a3fc9f3bcb7c8e3806f045a
alanagoyal <alanaan@gmail.com>
2024-07-07 15:12:30 -0700

    remove og from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,17 +34,9 @@ export default async function RootLayout({
         <meta property="twitter:card" content="summary_large_image"></meta>
         <meta property="twitter:title" content={siteConfig.name}></meta>
         <meta property="twitter:description" content={siteConfig.description}></meta>
-        <meta
-          property="twitter:image"
-          content={`${siteConfig.og}`}
-        ></meta>
         <meta property="og:site_name" content={siteConfig.name}></meta>
         <meta property="og:description" content={siteConfig.description}></meta>
         <meta property="og:title" content={siteConfig.name}></meta>
-        <meta
-          property="og:image"
-          content={`${siteConfig.url}`}
-        />
         <meta property="og:url" content={siteConfig.url}></meta>
       </head>
       <body
