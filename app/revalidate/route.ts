e2397ecf8d84ab6808a8f7050f412681f47541f9
alanagoyal <alanaan@gmail.com>
2024-07-09 08:59:55 -0700

    adjust revalidate

--- app/revalidate/route.ts
+++ app/revalidate/route.ts
@@ -1,17 +1,24 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { revalidatePath } from 'next/cache';
+import { NextRequest, NextResponse } from "next/server";
+import { revalidatePath } from "next/cache";
 
 export async function POST(request: NextRequest) {
-  const { slug } = await request.json();
-  
-  if (!slug) {
-    return NextResponse.json({ message: 'Missing slug parameter' }, { status: 400 });
-  }
-
   try {
+    const { slug } = await request.json();
+
+    if (!slug) {
+      return NextResponse.json(
+        { message: "Missing slug parameter" },
+        { status: 400 }
+      );
+    }
+
     revalidatePath(`/${slug}`);
     return NextResponse.json({ revalidated: true, now: Date.now() });
   } catch (err) {
-    return NextResponse.json({ message: 'Error revalidating' }, { status: 500 });
+    console.error(err);
+    return NextResponse.json(
+      { message: "Error revalidating" },
+      { status: 500 }
+    );
   }
 }
