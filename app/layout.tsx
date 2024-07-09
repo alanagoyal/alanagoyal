26aef7480a9344edea4b5e8868d5c38d48d5a9c7
Ankur Goyal <ankrgyl@gmail.com>
2024-07-09 15:33:30 -0700

    Query session notes separately

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,10 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const supabase = createBrowserClient();
-  const { data: notes } = await supabase.from("notes").select("*").limit(10000);
+  const { data: notes } = await supabase
+    .from("notes")
+    .select("*")
+    .eq("public", true);
 
   return (
     <html lang="en">
