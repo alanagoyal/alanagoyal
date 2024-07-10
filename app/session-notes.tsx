be9996f4c82751f1a14988a1221ce04e42578a4d
alanagoyal <alanaan@gmail.com>
2024-07-10 09:04:13 -0700

    add rpc calls for select queries

--- app/session-notes.tsx
+++ app/session-notes.tsx
@@ -67,9 +67,10 @@ async function getSessionNotes({
   supabase: SupabaseClient;
   sessionId: string;
 }) {
-  const { data: notes } = await supabase
-    .from("notes")
-    .select("*")
-    .eq("session_id", sessionId);
+
+  const { data : notes } = await supabase.rpc("select_session_notes", {
+    note_session_id: sessionId
+  });
+
   return notes;
 }
