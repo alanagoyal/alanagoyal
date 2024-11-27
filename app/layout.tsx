dd238b098813f39b787403c87a716e80280dea8e
alanagoyal <alanaan@gmail.com>
2024-11-26 19:11:14 -0800

    add more metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,34 @@
 import { Metadata } from "next";
-import { ThemeProvider } from "@/components/theme-provider"
-import "./globals.css"
+import { ThemeProvider } from "@/components/theme-provider";
+import "./globals.css";
 import { siteConfig } from "@/config/site";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteConfig.url),
-  title: siteConfig.name, 
+  title: siteConfig.name,
   description: siteConfig.description,
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <title>{siteConfig.name}</title>
+        <meta property="twitter:card" content="summary_large_image"></meta>
+        <meta property="twitter:title" content={siteConfig.name}></meta>
+        <meta
+          property="twitter:description"
+          content={siteConfig.description}
+        ></meta>
+        <meta property="og:site_name" content={siteConfig.name}></meta>
+        <meta property="og:description" content={siteConfig.description}></meta>
+        <meta property="og:title" content={siteConfig.name}></meta>
+        <meta property="og:url" content={siteConfig.url}></meta>
+      </head>
       <body className="h-screen">
         <ThemeProvider
           attribute="class"
@@ -27,5 +40,5 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
