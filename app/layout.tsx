629c57e04b5941f5f79fa507e56a95e886a38e6a
alanagoyal <alanaan@gmail.com>
2024-12-22 13:39:42 -0800

    fix twitter image

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: siteConfig.name,
     description: siteConfig.description,
-    images: ["/og-image"],
+    images: ["/api/og"],
   },
 };
 
