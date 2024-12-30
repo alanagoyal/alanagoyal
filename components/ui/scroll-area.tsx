374313bb3838b7fe35e83f103834d46b352878fc
alanagoyal <alanaan@gmail.com>
2024-12-30 09:42:34 -0800

    update scrollbar

--- components/ui/scroll-area.tsx
+++ components/ui/scroll-area.tsx
@@ -6,8 +6,10 @@ import { cn } from "@/lib/utils"
 
 const ScrollArea = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root> & {
+    withVerticalMargins?: boolean
+  }
+>(({ className, children, withVerticalMargins = false, ...props }, ref) => (
   <ScrollAreaPrimitive.Root
     ref={ref}
     className={cn("relative overflow-hidden", className)}
@@ -16,23 +18,31 @@ const ScrollArea = React.forwardRef<
     <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
       {children}
     </ScrollAreaPrimitive.Viewport>
-    <ScrollBar />
+    <ScrollBar withVerticalMargins={withVerticalMargins} />
     <ScrollAreaPrimitive.Corner />
   </ScrollAreaPrimitive.Root>
 ))
 ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName
 
+// Add vertical margins to for chat area component
 const ScrollBar = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>
->(({ className, orientation = "vertical", ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar> & {
+    withVerticalMargins?: boolean
+  }
+>(({ className, orientation = "vertical", withVerticalMargins = false, ...props }, ref) => (
   <ScrollAreaPrimitive.ScrollAreaScrollbar
     ref={ref}
     orientation={orientation}
     className={cn(
-      "flex touch-none select-none transition-colors",
+      "flex touch-none select-none transition-all duration-300",
+      "opacity-80 hover:opacity-100",
+      "bg-transparent hover:border-l hover:border-gray-200 dark:hover:border-gray-700",
       orientation === "vertical" &&
-        "h-full w-[14px]",
+        cn(
+          "w-[10px] hover:w-[14px]",
+          withVerticalMargins ? "h-[calc(100%-8rem)] my-16" : "h-full"
+        ),
       orientation === "horizontal" &&
         "h-2.5 flex-col border-t border-t-transparent p-[1px]",
       className
@@ -42,9 +52,8 @@ const ScrollBar = React.forwardRef<
     <ScrollAreaPrimitive.ScrollAreaThumb 
       className={cn(
         "relative flex-1 rounded-full transition-colors duration-200",
-        "border-4 border-solid border-transparent bg-clip-padding",
-        "bg-[rgba(0,0,0,0.2)] dark:bg-[rgba(255,255,255,0.2)]",
-        "hover:bg-[rgba(0,0,0,0.3)] dark:hover:bg-[rgba(255,255,255,0.3)]"
+        "border-2 border-solid border-transparent bg-clip-padding",
+        "bg-gray-500 dark:bg-gray-400"
       )} 
     />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
