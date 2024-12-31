c4c835320375fdc43de67636e792cda5830e9e86
alanagoyal <alanaan@gmail.com>
2024-12-31 09:06:36 -0800

    fix padding on nav

--- components/nav.tsx
+++ components/nav.tsx
@@ -32,7 +32,7 @@ export function Nav({ onNewChat, isMobileView }: NavProps) {
 
   return (
     <>
-      <div className="py-2 px-2 bg-background flex items-center justify-between bg-muted sticky top-0 z-[1]">
+      <div className="px-4 py-2 bg-background flex items-center justify-between bg-muted sticky top-0 z-[1]">
         <div className="flex items-center gap-1.5">
           <div className="w-3 h-3 rounded-full bg-red-500" />
           <div className="w-3 h-3 rounded-full bg-yellow-500" />
