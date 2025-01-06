0cab6bed8a354c8a444b717464a6f735eee5393d
alanagoyal <alanaan@gmail.com>
2025-01-05 17:06:49 -0800

    add extra spacer for reactions

--- components/message-bubble.tsx
+++ components/message-bubble.tsx
@@ -194,13 +194,13 @@ export function MessageBubble({
   };
 
   return (
-    <div
-      className={cn(
-        "flex w-full flex-col relative z-10"
-        // Align messages based on sender
+    <div className="flex w-full flex-col relative z-10">
+      {/* Spacer before messages */}
+      <div className="h-1 bg-background" />
+      {/* Extra space between messages with reactions */}
+      {message.reactions && message.reactions.length > 0 && (
+        <div className="h-2 bg-background" />
       )}
-    >
-      <div className="h-3 bg-background" />
 
       {/* Show recipient name for messages from others */}
       {recipientName && (
@@ -210,6 +210,7 @@ export function MessageBubble({
       )}
 
       <div className="flex">
+        {/* Left spacer for blue messages */}
         {isMe && <div className="flex-1 bg-background" />}
         {/* Message bubble container */}
         {isSystemMessage ? (
@@ -390,15 +391,18 @@ export function MessageBubble({
             </div>
           </div>
         )}
+        {/* Right spacer for gray messages */}
         {!isSystemMessage && !isMe && <div className="flex-1 bg-background" />}
       </div>
+
       {/* Show "Delivered" for last message from current user */}
       {isMe && isLastUserMessage && !isTyping && (
         <div className="text-[10px] text-gray-500 pt-1 pr-1 bg-background text-right">
           <span className={cn(justSent && "animate-scale-in")}>Delivered</span>
         </div>
       )}
-      <div className="h-3 bg-background" />
+      {/* Spacer after messages */}
+      <div className="h-1 bg-background" />
     </div>
   );
 }
