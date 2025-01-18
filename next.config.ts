011d1f6f62f8580c4195083860a8b87bdc1cc299
alanagoyal <alanaan@gmail.com>
2025-01-18 09:23:17 -0800

    domain stuff

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/messages",
+  assetPrefix: "/messages",
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
